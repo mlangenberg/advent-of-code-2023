61997ff72d170bd26ed164c21717e711697fb5dc
Matthijs Langenberg <matthijs.langenberg@nedap.com>
2023-12-07 21:36:37 +0100

    day 5

--- day-05/part2.rb
+++ day-05/part2.rb
@@ -1,77 +1,37 @@
-class Range
-  def overlaps?(other)
-    cover?(other.first) || other.cover?(first)
-  end
-end
-
 MapRange = Data.define(:dest_start, :src_start, :length) do
-  def convert(range)
+  def convert(numbers)
     Range.new(
-      dest_start - src_start + range.begin,
-      dest_start - src_start + range.end
-    ).tap do |output|
-      # puts "INPUT: #{range}"
-      # puts "OUTPUT: #{output}"
-    end
+      dest_start - src_start + numbers.begin,
+      dest_start - src_start + numbers.end
+    )
   end
 
   def begin = src_start
   def end = src_start + length - 1
-  def delta
-    i = dest_start - src_start
-    i.positive? ? "+#{i}" : i.to_s
-  end
 end
 
-# 20..100
-# range 30..150
-
-# 20...30
-# 30...100
-
-# 20..200
-# range 30..150
-#
-# 20...30
-# 30..150
-# 151..200
-
 Map = Struct.new(:ranges) do
-  def [](number_range)
-    puts "\tCalled with #{number_range}"
-    current = number_range
+  def [](numbers)
     output = []
     ranges.sort_by(&:begin).each do |range|
-      break if current.end < range.begin
+      break if numbers.end < range.begin
 
-      if current.begin < range.begin
-        print "H<#{current.begin..range.begin - 1}> "
-        output << (current.begin..range.begin - 1)
-        current = range.begin..current.end
+      if numbers.begin < range.begin
+        output << (numbers.begin..range.begin - 1)
+        numbers = range.begin..numbers.end
       end
-      overlap = [current.begin, range.begin].max..[current.end, range.end].min
-      if overlap.begin <= overlap.end
-        output << range.convert(overlap).tap { |output| print "M<#{overlap}> " }
-      end
-      if range.end >= current.begin
-        current = (range.end+1)..current.end
-      end
+      overlap = [numbers.begin, range.begin].max..[numbers.end, range.end].min
+      output << range.convert(overlap) if overlap.begin <= overlap.end
+      numbers = (range.end + 1)..numbers.end if range.end >= numbers.begin
     end
-    if current.begin <= current.end
-      print "T<#{current}> "
-      output << current
-    else
-      print "X<#{current}>"
-    end
-    puts
-    output.sort_by(&:begin).tap { puts "\tReturning #{_1}" }
+    output << numbers if numbers.begin <= numbers.end
+    output
   end
 end
 
 sections = ARGF.read.split("\n\n")
 seed_ranges = sections.shift.split(':').last.split.map(&:to_i)
                       .each_slice(2).map { |start, length| (start..start + length - 1) }
-
 
 maps = {}
 sections.each do |section|
@@ -82,25 +42,14 @@ end
 
 categories = %w[seed soil fertilizer water light temperature humidity location]
 
-locations = seed_ranges.map do |seeds|
-  puts "***************************"
-  lowest_location = Float::INFINITY
-
+seed_ranges.map do |seeds|
   ranges = [seeds]
   categories[...-1].each_with_index do |category, index|
     map = maps["#{category}-to-#{categories[index + 1]}"]
-    puts "Using #{category} to #{categories[index + 1]}"
     ranges = ranges.map do |range|
       map[range]
-    end.flatten.sort_by(&:begin).tap { puts "> Output for next map: #{_1}\n" }
+    end.flatten
   end
 
-  lowest_location = ranges.map(&:begin).min if ranges.map(&:begin).min < lowest_location
-  lowest_location
-end
-puts
-puts locations.min
-puts
-
-
-# puts maps["light-to-temperature"][(157606391..357109335)].inspect
+  ranges.min_by(&:begin).begin
+end.min.then { puts "Lowest location: #{_1}" }
