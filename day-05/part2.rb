e69476cf9c6d05fa3970dc5d6b80be905218edce
Matthijs Langenberg <matthijs.langenberg@nedap.com>
2023-12-07 14:17:51 +0100

    day 5 WIP

--- day-05/part2.rb
+++ day-05/part2.rb
@@ -1,20 +1,61 @@
+class Range
+  def overlaps?(other)
+    cover?(other.first) || other.cover?(first)
+  end
+end
+
 MapRange = Data.define(:dest_start, :src_start, :length) do
-  def convert(number)
-    dest_start - src_start + number
+  def convert(range)
+    Range.new(
+      dest_start - src_start + range.begin,
+      dest_start - src_start + range.end
+    ).tap do |output|
+      puts "INPUT: #{range}"
+      puts "OUTPUT: #{output}"
+    end
   end
 
   def cover?(number)
     (src_start...(src_start + length)).cover?(number)
   end
+
+  def begin = src_start
+  def end = src_start + length - 1
 end
 
+# 20..100
+# range 30..150
+
+# 20...30
+# 30...100
+
+# 20..200
+# range 30..150
+#
+# 20...30
+# 30..150
+# 151..200
+
 Map = Struct.new(:ranges) do
-  def [](number)
-    if (range = ranges.find { _1.cover?(number) })
-      range.convert(number)
-    else
-      number
+  def [](number_range)
+    puts "\tCalled with #{number_range}"
+    current = number_range
+    output = []
+    ranges.sort_by(&:begin).each do |range|
+      if current.begin < range.begin
+        puts "Unmodified start #{(current.begin...range.begin)}"
+        output << (current.begin...range.begin)
+        current = range.begin..current.end
+      end
+      overlap = [current.begin, range.begin].max..[current.end, range.end].min
+      if overlap.begin <= overlap.end
+        puts "Match with #{range} (#{overlap})"
+        output << range.convert(overlap)
+      end
+      current = (range.end + 1)..current.end if range.end >= current.begin
     end
+    output << current if current.begin <= current.end
+    output.sort_by(&:begin).tap { puts "\tReturning #{_1}" }
   end
 end
 
@@ -31,19 +72,22 @@ end
 
 categories = %w[seed soil fertilizer water light temperature humidity location]
 
-locations = seed_ranges.map do |seeds|
-  Ractor.new(seeds, categories, maps) do |seeds, categories, maps|
-    lowest_location = Float::INFINITY
-    seeds.each do |seed|
-      number = seed
-      categories.each_with_index do |category, index|
-        if (map = maps["#{category}-to-#{categories[index + 1]}"])
-          number = map[number]
-        end
-      end
-      lowest_location = number if number < lowest_location
-    end
-    lowest_location
-  end
-end
-puts locations.map(&:take).min
+# locations = seed_ranges.map do |seeds|
+#   puts "***************************"
+#   lowest_location = Float::INFINITY
+
+#   ranges = [seeds]
+#   categories[...-1].each_with_index do |category, index|
+#     map = maps["#{category}-to-#{categories[index + 1]}"]
+#     puts "Using #{category} to #{categories[index + 1]}"
+#     ranges = ranges.map do |range|
+#       map[range]
+#     end.flatten
+#   end
+
+#   lowest_location = ranges.map(&:begin).min if ranges.map(&:begin).min < lowest_location
+#   lowest_location
+# end
+# puts locations.min
+puts
+puts maps["humidity-to-location"][1257741823..1384364697].inspect
