0328114748ed04087871ee3fe326fb70339b6460
Matthijs Langenberg <matthijs.langenberg@nedap.com>
2023-12-11 20:43:47 +0100

    day 8

---
+++ day-08/part1.rb
@@ -0,0 +1,15 @@
+instructions = ARGF.readline.chomp.chars.map { _1 == 'L' ? 0 : 1 }.cycle
+ARGF.readline # skip empty line
+nodes = {}
+ARGF.readlines.each do |line|
+  element, left, right = line.scan(/\w+/)
+  nodes[element] = [left, right]
+end
+
+steps = 0
+current = 'AAA'
+until current == 'ZZZ'
+  steps += 1
+  current = nodes[current][instructions.next]
+end
+p steps
