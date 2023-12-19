8b7123bc645ae3c811f1115d2849fcdfe281c5c0
Matthijs Langenberg <matthijs.langenberg@nedap.com>
2023-12-19 14:21:07 +0100

    day 10

--- day-10/part1.rb
+++ day-10/part1.rb
@@ -1,57 +1,51 @@
 class Maze
-  UP = [-1, 0]
-  DOWN = [1, 0]
-  LEFT =  [0, -1]
-  RIGHT = [0, 1]
-  MOVES = [UP, DOWN, LEFT, RIGHT]
+  UP = [-1, 0].freeze
+  DOWN = [1, 0].freeze
+  LEFT =  [0, -1].freeze
+  RIGHT = [0, 1].freeze
+  MOVES = [UP, DOWN, LEFT, RIGHT].freeze
   NEXT_MOVES = {
     UP => { '7' => LEFT, 'F' => RIGHT },
     DOWN => { 'L' => RIGHT, 'J' => LEFT },
     LEFT => { 'L' => UP, 'F' => DOWN },
     RIGHT => { 'J' => UP, '7' => DOWN }
-  }
-
+  }.freeze
+  TILES_FOR_START_MOVE = {
+    UP => %w[| 7 F],
+    DOWN => %w[| L J],
+    LEFT => %w[- L F],
+    RIGHT => %w[- J 7]
+  }.freeze
   attr_reader :steps
 
   def initialize(tiles)
     @tiles = tiles
     @pos_y = tiles.find_index { _1.include?('S') }
     @pos_x = tiles[@pos_y].index('S')
-    @direction = MOVES.find { valid_start_move?(_1) }
+    @direction = MOVES.find { TILES_FOR_START_MOVE.fetch(_1, []).include?(next_tile(_1)) }
     @steps = 0
-  end
-
-  def valid_start_move?(move)
-    next_pos = [[@pos_y, @pos_x], move].transpose.map(&:sum)
-    next_value = @tiles.dig(*next_pos) || '.'
-    case move
-    when UP
-      %w[| 7 F].include?(next_value)
-    when DOWN
-      %w[| L J].include?(next_value)
-    when LEFT
-      %w[- L F].include?(next_value)
-    when RIGHT
-      %w[- J 7].include?(next_value)
-    else
-      false
-    end
   end
 
   def move
     @steps += 1
-    @pos_y, @pos_x = [[@pos_y, @pos_x], @direction].transpose.map(&:sum)
+    @pos_y, @pos_x = next_position
     @direction = NEXT_MOVES[@direction].fetch(current_tile, @direction)
   end
 
   def current_tile
     @tiles.dig(@pos_y, @pos_x)
   end
+
+  def next_tile(move)
+    @tiles.dig(*next_position(move))
+  end
+
+  def next_position(move = @direction)
+    [[@pos_y, @pos_x], move].transpose.map(&:sum)
+  end
 end
 
 maze = Maze.new(ARGF.readlines.map(&:chomp).map(&:chars))
 maze.move
-until maze.current_tile == 'S'
-  maze.move
-end
+maze.move until maze.current_tile == 'S'
 puts maze.steps
